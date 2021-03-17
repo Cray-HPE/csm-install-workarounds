7787c97e540e989eea5509a2d937aad10c4a514e
Jacob Salmela <jacob.salmela@hpe.com>
2021-03-17 09:18:45 -0500

    CASMINST-1813 fix broken echo command and add readme for this workaround
    Signed-off-by: Jacob Salmela <jacob.salmela@hpe.com>

--- workarounds/before-ncn-boot/CASMINST-1747/casminst-1747.sh
+++ workarounds/before-ncn-boot/CASMINST-1747/casminst-1747.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+echo "Checking compatibility..."
+if [[ $HOSTNAME =~ .*pit.* ]]; then
+  echo "OK"
+else
+  echo "This workaround script should only be run on the PIT."
+  exit 1
+fi
+
 grep disk-opts /var/www/boot/script.ipxe
 sed -i 's/disk-opts /disk-opts metal.disk.conlib.size=40 metal.disk.k8slet.size=10 /g' /var/www/boot/script.ipxe
 grep disk-opts /var/www/boot/script.ipxe
 /root/bin/set-sqfs-links.sh
 echo boot parameters adjusted for new containerd partition size
-echo Completed CASMINST-1747
-
-metal.disk.conlib.size=40 metal.disk.k8slet.size=10
+echo "Completed CASMINST-1747 metal.disk.conlib.size=40 metal.disk.k8slet.size=10"
