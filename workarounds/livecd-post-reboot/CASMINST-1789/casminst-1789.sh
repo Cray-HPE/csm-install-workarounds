d4051a7c980464d7297281d291db8a3591fa877b
Jacob Salmela <jacob.salmela@hpe.com>
2021-03-19 08:37:42 -0500

    CASMINST-1800 remove CASMINST-1789 workaround now that permanent fix is implemented
    Signed-off-by: Jacob Salmela <jacob.salmela@hpe.com>

--- workarounds/livecd-post-reboot/CASMINST-1789/casminst-1789.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Sets kernel parameters TCP memory
-# notifies every host of the setting.
-# temporary/running system
-pdsh -b -w $(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u |  tr -t '\n' ',') 'sysctl -w net.ipv4.tcp_mem="3083247 4110996 6166494"'
-
-# persist
-pdsh -b -w $(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u |  tr -t '\n' ',') 'echo net.ipv4.tcp_mem="3083247 4110996 6166494" > /etc/sysctl.d/999-tcp-mem.conf'
