ea8d4c32f30d8bf950af034db30b9036aff67046
Jeanne Ohren <johren@cray.com>
2021-03-30 17:28:22 -0500

    CASMINST-1871:  Remove CASMINST-1735 WAR

--- workarounds/livecd-post-reboot/CASMINST-1735/casminst-1735.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pdsh -b -w $(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u |  tr -t '\n' ',') 'cp -pv /srv/cray/resources/metal/sshd_config /etc/ssh/ && echo "TrustedUserCAKeys /etc/ssh/trusted-user-ca-keys.pem" >> /etc/ssh/sshd_config && systemctl --signal HUP restart sshd'
-echo HANGUP was sent to sshd on all NCNs to reload the new sshd_config
-echo This is the end of CASMINST-1735
