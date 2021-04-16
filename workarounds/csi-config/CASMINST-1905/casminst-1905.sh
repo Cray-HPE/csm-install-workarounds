a3c5d39bf5837896f8213350864eb95f18bc4b23
Jeanne Ohren <johren@cray.com>
2021-04-16 12:48:39 -0500

    CASMINST-1948:  fix CAN range in networks/CAN.yaml and sls_input_file.json as well

--- workarounds/csi-config/CASMINST-1905/casminst-1905.sh
+++ workarounds/csi-config/CASMINST-1905/casminst-1905.sh
@@ -22,6 +22,11 @@ if [ ! -r ${csidir}/dnsmasq.d/CAN.conf ]; then
    exit 1
 fi
 
+if [ ! -r ${csidir}/sls_input_file.json ]; then
+   echo "ERROR: ${csidir}/sls_input_file.json not found or not readable"
+   exit 1
+fi
+
 new_range_end=$(grep can-static-pool ${csidir}/system_config.yaml | awk '{print $2}' | cut -d'/' -f 1)
 if [ -z ${new_range_end} ]; then
     echo "ERROR:  Could not determine new range end"
@@ -37,8 +42,26 @@ fi
 echo "Replacing ${old_range_end} with ${new_range_end} in dhcp-range for CAN"
 
 sed -i "/dhcp-range/s/${old_range_end}/${new_range_end}/g" ${csidir}/dnsmasq.d/CAN.conf
+if [ $? -eq 0 ]; then
+    echo "Replaced range end in dnsmasq.d/CAN.conf"
+else
+    echo "Error replacing range end in dnsmasq.d/CAN.conf"
+fi
 
-echo "Restarting dnsmasq..."
-systemctl restart dnsmasq.service
+sed -i "/iprange-end/s/${old_range_end}/${new_range_end}/g" ${csidir}/networks/CAN.yaml
+if [ $? -eq 0 ]; then
+    echo "Replaced range end in networks/CAN.yaml"
+else
+    echo "Error replacing range end in networks/CAN.yaml"
+fi
+
+mv ${csidir}/sls_input_file.json ${csidir}/sls_input_file.json.pre-CASMINST-1905
+jq --arg inp1 "${new_range_end}" 'if .Networks.CAN.ExtraProperties.Subnets[2].DHCPEnd? then .Networks.CAN.ExtraProperties.Subnets[2].DHCPEnd=$inp1 else . end' ${csidir}/sls_input_file.json.pre-CASMINST-1905 > ${csidir}/sls_input_file.json
+
+if [ $? -eq 0 ]; then
+    echo "Replaced range end in sls_input_file.json"
+else
+    echo "Error replacing range end in sls_input_file.json"
+fi
 
 exit 0
