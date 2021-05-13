097e1be5fc4b5b3f50b343109fc847b0da3a9ec0
Russell Bunch <rbunch@cray.com>
2021-05-13 18:44:00 -0500

    fix CASMINST-1612.sh so it can run on storage too
    CASMINST-2253 Remove the check for 'already ran'.   We need to be able to run again if another node is rebuilt.

--- workarounds/livecd-post-reboot/CASMINST-1612/CASMINST-1612.sh
+++ workarounds/livecd-post-reboot/CASMINST-1612/CASMINST-1612.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
+echo Opening and refreshing fallback artifacts on the NCNs..
+pdsh -b -w $(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u |  tr -t '\n' ',') '
+set -x
 mount -L BOOTRAID /tmp/mount
-curl -o /tmp/mount/boot/initrd.img.xz http://rgw-vip.nmn/ncn-images/k8s-initrd.img.xz
-curl -o /tmp/mount/boot/kernel http://rgw-vip.nmn/ncn-images/k8s-kernel
+if [[ $(hostname) =~ 'ncn-s' ]]; then
+	curl -o /tmp/mount/boot/initrd.img.xz http://rgw-vip.nmn/ncn-images/ceph-initrd.img.xz
+	curl -o /tmp/mount/boot/kernel http://rgw-vip.nmn/ncn-images/ceph-kernel
+else
+	curl -o /tmp/mount/boot/initrd.img.xz http://rgw-vip.nmn/ncn-images/k8s-initrd.img.xz
+	curl -o /tmp/mount/boot/kernel http://rgw-vip.nmn/ncn-images/k8s-kernel
+fi
 umount /tmp/mount
+'
+echo done
--- workarounds/livecd-post-reboot/CASMINST-1612/README.md
+++ workarounds/livecd-post-reboot/CASMINST-1612/README.md
@@ -1,4 +1,4 @@
 # Copy Kernel/Initrd Into Place
 
-After m001 is rebooted and has joined the cluster it is necessary to copy the kernel and initrd into place 
+After an NCN is rebooted and has joined the cluster it is necessary to copy the kernel and initrd into place 
 by running the `CASMINST-1612.sh` script on m001.
