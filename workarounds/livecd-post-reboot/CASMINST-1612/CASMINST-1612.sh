b92fbb604412ee7dc51a0349204022a918104f76
Russell Bunch <rbunch@cray.com>
2021-05-28 05:48:50 -0500

    CASMTRAIGE-1309 Copy locally for offline support
    Support cases where we have no working s3.

--- workarounds/livecd-post-reboot/CASMINST-1612/CASMINST-1612.sh
+++ workarounds/livecd-post-reboot/CASMINST-1612/CASMINST-1612.sh
@@ -5,14 +5,11 @@ set -e
 echo Opening and refreshing fallback artifacts on the NCNs..
 pdsh -b -w $(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u |  tr -t '\n' ',') '
 set -x
-mount -L BOOTRAID /tmp/mount
-if [[ $(hostname) =~ 'ncn-s' ]]; then
-	curl -o /tmp/mount/boot/initrd.img.xz http://rgw-vip.nmn/ncn-images/ceph-initrd.img.xz
-	curl -o /tmp/mount/boot/kernel http://rgw-vip.nmn/ncn-images/ceph-kernel
-else
-	curl -o /tmp/mount/boot/initrd.img.xz http://rgw-vip.nmn/ncn-images/k8s-initrd.img.xz
-	curl -o /tmp/mount/boot/kernel http://rgw-vip.nmn/ncn-images/k8s-kernel
-fi
+mkdir -pv /tmp/mount
+mount -L BOOTRAID /tmp/mount/
+[ -d /tmp/mount/boot ] || echo >&2 BOOTRAID is missing grub and more && exit 1
+cp -pv /run/initramfs/live/LiveOS/initrd.img.xz /tmp/mount/boot
+cp -pv /run/initramfs/live/LiveOS/kernel /tmp/mount/boot/
 umount /tmp/mount
 '
 echo done
